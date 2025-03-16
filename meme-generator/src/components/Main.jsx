3d7b9591e472882225e3d5ef757dc85a695c133e
Anri20 <88647934+Anri20@users.noreply.github.com>
2025-03-16 08:59:30 +0700

    Fetch Meme from API

--- meme-generator/src/components/Main.jsx
+++ meme-generator/src/components/Main.jsx
@@ -8,6 +8,14 @@ export default function Main() {
         imageUrl: "http://i.imgflip.com/1bij.jpg"
     })
 
+    const [allMemes, setAllMemes] = useState([])
+
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
+
     function handleChange(event) {
         const { value, name } = event.currentTarget
         setMeme(prevMeme => ({
